e5c01511ae2fe395c31835505a43f3bdca22a1ba
Iswinsyah <iswinsyah@example.com>
2025-12-25 15:48:41 +0700

    Debug: Force error reporting on schema v15

--- sadigs/sadigs_api_v3/update_schema_v15.php
+++ sadigs/sadigs_api_v3/update_schema_v15.php
@@ -1,4 +1,9 @@
 <?php
+// --- DEBUGGING: Force display errors ---
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'db_connect.php';
 
 try {
